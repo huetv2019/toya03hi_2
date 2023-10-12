10f3a94915bd63a431e27cc489e0f5a1db967437
huetv <trankylong@gmail.com>
2023-10-12 22:00:59 +0700

    .

--- s00_bailam.py
+++ s00_bailam.py
@@ -31,5 +31,9 @@ hi('1', '22', '333', '4444') | Hi 1, 22, 333 and 4444!
 
 #region bailam
 def hi(name):
+  if name == '':
+    return 'Hi!'
   return f'Hi {name}!'
 #endregion bailam
+
+print(hi(''))
